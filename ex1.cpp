6f23b2a96e17bb91804f96edcbc9477570324fe8
Baudouin Bosc <baudouin.bosc@gmail.com>
2024-10-24 11:50:12 +0200

    done with the first 20 exercises

--- ex1.cpp
+++
@@ -1,9 +0,0 @@
-// 1. Write a program in C++ to print welcome text on a separate line. 
-#include <iostream>
-
-int main()
-{
-    std::cout << "first line\n";
-    std::cout << "second line\n";
-    return 0;
-}
